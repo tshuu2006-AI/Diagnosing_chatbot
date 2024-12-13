401767abbe051b34ded1962198acc74fce00664e
Nthuu-AI <tshuu2006@gmail.com>
2024-12-14 03:01:54 +0700

    commit

--- Data_prepare.py
+++ Data_prepare.py
@@ -1,12 +1,13 @@
+#import thư viện
 import json
 import pandas as pd
 
 #Load the new unlabled intents and responses to dataframe
 with open("unlableled_intents.json", "r",encoding="utf-8") as file:
-  intents = json.load(file)
+  intents = json.load(file) #load các intents đã xử lý
 
 with open("responses.json","r",encoding="utf") as file:
-  responses = json.load(file)
+  responses = json.load(file)#load các responses đã xử lý
 
 responses_keys  = []
 intent_keys = list(intents.keys())
@@ -15,27 +16,32 @@ response_values = list(responses.values())
 for intent_key in intent_keys:
   responses_keys.append(f"utter_ans_{intent_key}")
 
-
+#Tạo dataframe cho các intent và response
 Data = {"intent_keys": intent_keys,
         "intent_values": intent_values,
         "response_keys": responses_keys,
         "response_values": response_values}
-DF = pd.DataFrame(Data)
+DF = pd.DataFrame(Data) 
 DF_size = len(intent_keys)
 
-#Insert new intents and responses to the rasa configuring file.
+
 if __name__ == "__main__":
-  with open("domain.yml","r") as domain:
+  #Lưu file domain.yml dưới dạng list
+  with open("domain.yml","r") as domain: 
     domain_data = domain.readlines()
 
-  mark_intents = domain_data.index("intents:\n") + 1
+  #đánh dấu vị trí của intent và thêm dữ liệu vào sau đó
+  mark_intents = domain_data.index("intents:\n") + 1 
   for intent_key in DF["intent_keys"].tolist():
     domain_data.insert(mark_intents,f"  - {intent_key}\n")
 
+  #đánh dấu vị trí của responses và thêm dữ liệu vào sau đó
   mark_responses = domain_data.index("responses:\n") + 1
   for i in range(DF_size):
-    domain_data.insert(mark_responses,f"  {responses_keys[i]}:\n  - text: \"{response_values[i]}\"\n\n")
+    for j in range(len(response_values[i])):
+      domain_data.insert(mark_responses,f"  {responses_keys[i]}:\n  - text: \"{response_values[i]}\"\n\n")
 
+  #Viết lại toàn bộ file domain.yml bằng dữ liệu mới
   with open("domain.yml","w",encoding="utf-8") as domain:
     domain.writelines(domain_data)
 
--- actions/actions.py
+++ actions/actions.py
@@ -3,6 +3,48 @@ from rasa_sdk.executor import CollectingDispatcher
 from typing import Any, Text, Dict, List
 from groq import Groq
 import json
+class ActionRecommendExercise(Action):
+    def name(self) -> str:
+        return "action_recommend_exercise"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict) -> List[Dict]:
+        muscle_group = tracker.get_slot("muscle_group")
+        flag = True
+        if muscle_group == "chest":
+            exercise = "Bench Press or Push-Up"
+            method = "To perform a bench press, lie on a flat bench with your feet flat on the floor and grip the barbell slightly wider than shoulder-width apart. Unrack the barbell and slowly lower it towards your chest, keeping your elbows tucked in. Press the barbell back up to the starting position. For push-ups, start in a plank position with your hands shoulder-width apart. Lower your body towards the ground, keeping your core engaged, and then push back up. Remember to warm up before starting, maintain proper form, listen to your body, and be patient as you build strength and muscle."
+            link = "https://www.youtube.com/watch?v=CCZGD55NxGo\nhttps://www.youtube.com/watch?v=-MAABwVKxok"
+ 
+        elif muscle_group == "leg" or muscle_group == "legs":
+            exercise = "Squat or Leg Press"
+            method = "To perform a squat, stand with your feet shoulder-width apart and your toes slightly pointed outward. Lower your hips back and down as if sitting in a chair, keeping your back straight and your core engaged. Push through your heels to return to the starting position. For a leg press, sit on the machine and place your feet on the platform shoulder-width apart. Push the platform away from you until your legs are fully extended, then slowly return to the starting position. Remember to maintain proper form and use a weight that is challenging but manageable."
+            link = "https://www.youtube.com/watch?v=gcNh17Ckjgg\nhttps://www.youtube.com/shorts/ahaJTts1f3s"
+        elif muscle_group == "core" or muscle_group == "abs":
+            exercise = "Plank or Deadlift"
+            method = "To perform a deadlift, stand with your feet hip-width apart in front of the barbell. Bend down and grip the barbell with an overhand grip, keeping your back straight and your core engaged. Lift the barbell by straightening your legs and hips, keeping the barbell close to your body. Slowly lower the barbell back to the ground. To do a plank, start in a push-up position with your forearms on the ground and your elbows directly under your shoulders. Hold this position, keeping your body in a straight line from your head to your heels"
+            link = "https://www.youtube.com/watch?v=TvxNkmjdhMM\nhttps://www.youtube.com/watch?v=XxWcirHIwVo"
+        elif muscle_group == "back":
+            exercise = "Pull-ups or Seated Cable Rows"
+            method = "To perform a pull-up, grip the bar with an overhand grip, hands slightly wider than shoulder-width apart. Engage your core and pull your body up towards the 1  bar, bringing your chest to the bar. Slowly lower yourself back down to the starting position. To perform a seated cable row, sit at the cable machine with your feet flat on the floor. Grip the handle and lean back slightly, keeping your back straight. Pull the handle towards your chest, squeezing your shoulder blades together. Slowly return to the starting position."
+            link = "https://www.youtube.com/watch?v=bb8_5vZV5dU\nhttps://www.youtube.com/watch?v=vwHG9Jfu4sw"
+        elif muscle_group == "shoulder":
+            exercise = "Lateral Raises or Overhead Press"
+            method = "To perform a lateral raise, stand tall with your feet shoulder-width apart, holding dumbbells at your sides. Raise your arms out to the sides, keeping them slightly bent at the elbows, until they are parallel to the ground. Slowly lower your arms back down to the starting position. For an overhead press, stand with your feet shoulder-width apart, holding dumbbells at shoulder height. Press the dumbbells straight up overhead, keeping your elbows tucked in. Slowly lower the dumbbells back down to the starting position. Remember to maintain proper form and use a weight that is challenging but manageable."
+            link = "https://www.youtube.com/watch?v=PzsMitRdI_8\nhttps://www.youtube.com/watch?v=M2rwvNhTOu0"
+        else:
+            flag = False
+        if flag: 
+            text = f"You can try {exercise} for {muscle_group} building. {method}\n. Here are the links for better instruction:\n{link}"
+        else:
+            text = "I don't have the instructions for the muscle group that you want to train yet"
+        dispatcher.utter_message(
+            text=text
+        )
+        return []
+    
+
 class ActionLowConfidenceFallback(Action):
     def name(self) -> Text:
         return "action_low_confidence_fallback"
@@ -49,3 +91,4 @@ class ActionLowConfidenceFallback(Action):
       )
       response = chat_completion.choices[0].message.content
       return f"\n{response}"
+    
